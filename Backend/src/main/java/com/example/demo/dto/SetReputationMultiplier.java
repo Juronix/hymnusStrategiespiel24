f7f7880d7061022751f9b71e4890e08972bf7522
Jonathan Psenicka <jps@deron.de>
2024-08-24 19:48:45 +0200

    Add hymnen reset

---
+++ Backend/src/main/java/com/example/demo/dto/SetReputationMultiplier.java
@@ -0,0 +1,25 @@
+package com.example.demo.dto;
+
+public class SetReputationMultiplier {
+    private int id;
+    private double multiplier;
+
+    // Getter und Setter
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(double multiplier) {
+        this.multiplier = multiplier;
+    }
+}
